bfe44d4a2df3b91ae663c8e4940ded460e48ae0d
djmgit <djmdeveloper060796@gmail.com>
2017-09-12 03:28:16 +0530

    Added an API

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var things = require('./things.js');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
+var cookieParser = require('cookie-parser');
 var mongoose = require('mongoose');
+var session = require('express-session');
+var movies = require('./movies.js');
 
 // connecting db
 mongoose.connect('mongodb://localhost/my_db');
@@ -12,6 +15,8 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views','./views');
 
+app.use(cookieParser());
+app.use(session({secret: "Shh, its a secret!"}));
 
 // defining schema
 var personSchema = mongoose.Schema({
@@ -21,8 +26,22 @@ var personSchema = mongoose.Schema({
 });
 var Person = mongoose.model("Person", personSchema)
 
+app.get('/', function(req, res){
+   if(req.session.page_views){
+      req.session.page_views++;
+      res.send("You visited this page " + req.session.page_views + " times");
+   } else {
+      req.session.page_views = 1;
+      res.send("Welcome to this page for the first time!");
+   }
+});
+
 // example of using external routes
 app.use('/things', things);
+
+//Use the Router on the sub route /movies
+app.use('/movies', movies);
+
 
 // example of a simple get request
 app.get('/hello', function(req, res){
@@ -52,8 +71,11 @@ app.get('/components', function(req, res){
     res.render('content');
 });
 
+// for parsing post url payload
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// for handling uploads
 app.use(upload.array()); 
 app.use(express.static('public'));
 
@@ -104,10 +126,73 @@ app.get('/people', function(req, res){
    });
 });	
 
-// example of a middleware function
-app.use(function(req, res, next){
-   console.log("A new request received at " + Date.now());
-   next();
+var Users = [];
+
+// simple authentication system
+app.get('/signup', function(req, res){
+   res.render('signup');
+});
+
+app.post('/signup', function(req, res){
+   if(!req.body.id || !req.body.password){
+      res.status("400");
+      res.send("Invalid details!");
+   } else {
+      Users.filter(function(user){
+         if(user.id === req.body.id){
+            res.render('signup', {
+               message: "User Already Exists! Login or choose another user id"});
+         }
+      });
+      var newUser = {id: req.body.id, password: req.body.password};
+      Users.push(newUser);
+      req.session.user = newUser;
+      res.redirect('/protected_page');
+   }
+});
+function checkSignIn(req, res, next){
+   if(req.session.user){
+      next();     //If session exists, proceed to page
+   } else {
+      var err = new Error("Not logged in!");
+      console.log(req.session.user);
+      next(err);  //Error, trying to access unauthorized page!
+   }
+}
+app.get('/protected_page', checkSignIn, function (req, res){
+   res.render('protected_page', {id: req.session.user.id})
+});
+
+app.get('/login', function(req, res){
+   res.render('login');
+});
+
+app.post('/login', function(req, res){
+   console.log(Users);
+   if(!req.body.id || !req.body.password){
+      res.render('login', {message: "Please enter both id and password"});
+   } else {
+      Users.filter(function(user){
+         if(user.id === req.body.id && user.password === req.body.password){
+            req.session.user = user;
+            res.redirect('/protected_page');
+         }
+      });
+      res.render('login', {message: "Invalid credentials!"});
+   }
+});
+
+app.get('/logout', function(req, res){
+   req.session.destroy(function(){
+      console.log("user logged out.")
+   });
+   res.redirect('/login');
+});
+
+app.use('/protected_page', function(err, req, res, next){
+console.log(err);
+   //User should be authenticated! Redirect him to log in.
+   res.redirect('/login');
 });
 
 
